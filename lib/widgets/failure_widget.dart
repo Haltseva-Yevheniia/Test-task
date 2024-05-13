2514112677cbec11b73a2bfb7caa765c95b75c13
Haltseva-Yevheniia <evgeniya.extremeparty@gmail.com>
2024-05-13 11:54:00 +0200

    update empty response widget

--- lib/widgets/failure_widget.dart
+++ lib/widgets/failure_widget.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:riverpod_test_task/widgets/search_bar_widget.dart';
-
 
 import '../../../constants.dart';
 import '../../../style/style.dart';
-import '../../../widgets/project_widgets.dart';
 
 class SearchScreenFailureWidget extends StatelessWidget {
   const SearchScreenFailureWidget({
@@ -14,37 +10,12 @@ class SearchScreenFailureWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        dividerAppBar,
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SearchBarWidget(
-                  trailing: SvgPicture.asset('asset/icons/Close.svg'),
-                ),
-              ),
-              const Text(
-                emptySearchResultHeader,
-                textAlign: TextAlign.left,
-                style: FontStyles.headerMainAccent,
-              ),
-            ],
-          ),
-        ),
-        const Expanded(
-            child: Center(
-          child: Text(
-            emptySearchResultBody,
-            style: FontStyles.bodyPlaceHolder,
-            textAlign: TextAlign.center,
-          ),
-        ))
-      ],
+    return Center(
+      child: Text(
+        emptySearchResultBody,
+        style: FontStyles.bodyPlaceHolder,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
